12a4458e63a78b2feb1c15fd5117b2959597873a
Kebba Kanyi <kebbakanyi@gmail.com>
2018-06-06 00:22:18 -0700

    formatting

--- downloader/main.py
+++ downloader/main.py
@@ -1,10 +1,12 @@
-from pytube import YouTube
+#!/usr/local/bin/python
 import os
 import sys
+from pytube import YouTube
 
-PATHS = {
+
+PATH = {
     'HOME': os.path.expanduser("~"),
-    'DOWNLOADS': os.path.join(os.path.expanduser("~"), "Downloads")
+    # 'DOWNLOADS': os.path.join(os.path.expanduser("~"), "Downloads")
 }
 
 yes = 'yes y'.split()
@@ -21,8 +23,10 @@ def download_videos(input_links, destination):
 
 
 def prompt():
+
     while True:
-        response = input('Do you want to Download Youtube videos? (Y/N):  ').lower()
+        response = input(
+            'Do you want to Download Youtube videos? (Y/N):  ').lower()
         if response in yes:
             return True
         elif response in no:
@@ -32,27 +36,28 @@ def prompt():
 
 def get_destination_path():
 
-    print('Save Files in:')
+    print('Save Video(s) in:')
     while True:
         try:
-            response = int(input('1) Home Folder \n2) Make new Folder \n3) List Folders to chose from \n4) To exit \n'))
+            response = int(input('1) Home Folder \n2) Make new Folder \n3)'
+                                 ' List Folders \n4) To exit \n'))
 
             if response == 1:
-                return PATHS['HOME']
+                return PATH['HOME']
 
             elif response == 2:
                 folder_name = input('Enter Folder Name: ')
-                if not os.path.exists(os.path.join(PATHS['HOME'], folder_name)):
-                    os.makedirs(os.path.join(PATHS['HOME'], folder_name))
-                return os.path.join(PATHS['HOME'], folder_name)
+                if not os.path.exists(os.path.join(PATH['HOME'], folder_name)):
+                    os.makedirs(os.path.join(PATH['HOME'], folder_name))
+                return os.path.join(PATH['HOME'], folder_name)
 
             elif response == 3:
                 position = 0
                 folder = {}
-                for name in os.listdir(PATHS['HOME']):
-                    if os.path.isdir(os.path.join(PATHS['HOME'], name)) and not name.startswith('.'):
+                for name in os.listdir(PATH['HOME']):
+                    if os.path.isdir(os.path.join(PATH['HOME'], name)) and not name.startswith('.'):
                         position += 1
-                        folder[position] = os.path.join(PATHS['HOME'], name)
+                        folder[position] = os.path.join(PATH['HOME'], name)
                         print(position, name)
                 print('')
 
@@ -63,13 +68,14 @@ def get_destination_path():
                 sys.exit()
 
             else:
-                print('Input has to be either 1, 2 or 3')
+                print('Input has to be either 1, 2, 3 or 4')
 
         except ValueError:
             print('Wrong value')
 
 
 def get_youtube_links():
+
     links = []
     link_count = int(input('How many videos are you downloading? '))
 
@@ -96,16 +102,9 @@ def download():
             sys.exit()
         else:
             print('Wrong Input')
-
     else:
         print('Wrong input')
 
 
 if __name__ == '__main__':
     download()
-
-
-
-
-
-
